a387e953518a02c82c3630c5a75d76dc353232fa
simon liu <simonlyllyyy@gmail.com>
2020-08-06 15:44:32 +1000

    first version cimpleted

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,22 @@ const socket = io()
 
 const chatForm = document.getElementById("chat-form")
 const chatMessages = document.querySelector(".chat-messages")
+const roomName = document.getElementById("room-name")
+const userList = document.getElementById("users")
+
+//get username and room from url
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true, // ignore the & , = in the queries
+})
+
+//join chatroom
+socket.emit("joinRoom", { username, room })
+
+//get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room)
+  outputUsers(users)
+})
 
 // catch the message comming from the 'message channel' and print it to the google chrown console
 //Message channel from server
@@ -31,11 +47,23 @@ chatForm.addEventListener("submit", (e) => {
 function outputMessage(message) {
   const div = document.createElement("div") //create a div tag
   div.classList.add("message") //in chat html we find each div tag for chat message have a 'message' style class
-  div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
   <p class="text">
-    ${message}
+    ${message.text}
   </p>`
   document.querySelector(".chat-messages").appendChild(div)
   //found the tag with chat-messages class name
   // difference with getelementbyid is : getElementById is finding tag with id name; querySelector is with class name
+}
+
+// add room name to dom
+function outputRoomName(room) {
+  roomName.innerText = room
+}
+
+//add users to dom
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join("")}
+  `
 }
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const path = require("path")
 const http = require("http")
 const express = require("express")
 const socketio = require("socket.io")
+// setting up a wrapper for the message
+const formatMessage = require("./utils/messages")
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users")
 
 const app = express()
 const server = http.createServer(app)
@@ -9,26 +17,59 @@ const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, "public")))
 
+const botName = "chatBox bot"
+
 //run when client connet
 io.on("connection", (socket) => {
-  // To the single client
-  socket.emit("message", "Welcome to chat box")
-  // broadcast when a user connects
-  // this will broadcast to everyuser except the user thats connecting
-  socket.broadcast.emit("message", "a user has joined the chat")
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room)
+
+    socket.join(user.room)
+
+    // To the single client
+    socket.emit("message", formatMessage(botName, "Welcome to Chat Box"))
+
+    // broadcast when a user connects
+    // this will broadcast to everyuser except the user thats connecting
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joint the chat`)
+      )
+    //send user room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    })
+  })
 
   // broadcast to everyone including the user thats connecting
   // io.emit()
 
-  //Runs when client disconnect
-  socket.on("disconnect", () => {
-    io.emit("message", "a user has left the chat")
-  })
   //listen for chatMessage emit channel at the serverside
   socket.on("chatMessage", (msg) => {
+    //get the current user with the socket id
+    const user = getCurrentUser(socket.id)
     //then emit this messge from server to everyone
     // so all the message channel in main.js will catch the message
-    io.emit("message", msg)
+    io.to(user.room).emit("message", formatMessage(`${user.username}`, msg))
+  })
+
+  //Runs when client disconnect
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id)
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      )
+      // send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      })
+    }
   })
 })
 
